807ca6d7a0d04cee8766e693d1bd2a9fa365c827
uyiw <2623395289@qq.com>
2020-03-08 20:06:47 +0800

    首次

---
+++ src/view/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="shouye">
+    <commonHeader :bannerList="bannerList"></commonHeader>
+    <div class="shouye-type">
+      <router-link tag="div" to=""><img src="../assets/img/jingqu.png" />景区</router-link>
+      <router-link tag="div" to=""><img src="../assets/img/xiangsu.png" />乡宿</router-link>
+      <router-link tag="div" to=""><img src="../assets/img/canyin.png" />餐饮</router-link>
+      <router-link tag="div" to=""><img src="../assets/img/techan.png" />特产</router-link>
+    </div>
+  </div>
+</template>
+<script>
+import commonHeader from '../components/commonHeader'
+export default {
+  data() {
+    return {
+      bannerList: [
+        require('@/assets/img/2.png'),
+        require('@/assets/img/banner1.png'),
+        require('@/assets/img/2.png')
+      ],
+    }
+  },
+  components: {
+    commonHeader
+  }
+}
+</script>
+<style lang="scss">
+  @import '../assets/css/shouye.scss';
+</style>
+
